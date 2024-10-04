a78b60b1374a2df4ba2f4a1cb9383199b37e0209
Nicolas Benedetti Gonzalez <nicolas.benedettigonzalez@student.ucll.be>
2024-10-04 18:27:45 +0200

    changed yoda cleanup and incron config scripts

--- scripts/incron_config.sh
+++ scripts/incron_config.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 #
-find /etc -type d ! -regex '.*/.git.*' ! -name 'incron.d' -print0 | \
-xargs -0 -I{} echo "{} IN_CLOSE_WRITE,recursive=false /etc/scripts/backup.sh \$@/\$#" > /etc/incron.d/etc.conf
+## Find all directories in /etc, excluding .git and incron.d directories
+#
+find /etc -type d ! -regex '.*/\.git.*' ! -name 'incron.d' -print0 | while IFS= read -r -d '' dir; do
+	# Create an incron rule for each directory found
+	echo "$dir IN_CLOSE_WRITE,recursive=false /etc/scripts/backup.sh \$@/\$#" >> /etc/incron.d/etc.conf
+done
+
+#Restart the incron service to apply changes
 systemctl restart incron
+        
